dea00bf7a9a7af780322f7f3af4d1e0c8df6a5f4
Joshua Steffensmeier <jpsteffe@iastate.edu>
2012-10-18 18:36:55 -0500

    oops

--- src/Arduino/SnowplowArduino/commands.h
+++ src/Arduino/SnowplowArduino/commands.h
@@ -5,6 +5,6 @@
 #define CODE_SET_DIGITAL    0x02 // void (char pin, char b)
 #define CODE_READ_DIGITAL   0x03 // char (char pin)
 #define CODE_READ_ANALOG    0x04 // short (char pin)
-#define CODE_SET_PIN_INOUT  0x05 // void (char pin, char in/out)
+#define CODE_SET_PIN_MODE  0x05 // void (char pin, char in/out)
 
 #endif // __COMMANDS__
