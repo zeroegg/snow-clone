232b93b58d0d274d5ad8df6c4c4d3aa4d0654325
Joshua Steffensmeier <jpsteffe@iastate.edu>
2012-10-28 01:18:13 -0500

    simple test for the motor control

---
+++ src/Arduino/SnowplowArduino/motorControlTest/motorControlTest.ino
@@ -0,0 +1,21 @@
+int a, p; 
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600); 
+ pinMode(10,OUTPUT);
+ digitalWrite(10,LOW);
+ a = 1;
+ p = 1; 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  analogWrite(10, a);
+  if (a == 255 || a == 0)
+  { p *= -1;}
+  
+  a += p; 
+  
+  delay(50);  
+}
